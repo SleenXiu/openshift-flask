e492bc423be7c635e06024a488c1262b3fa20f88
sleen <xiushilin@hotmail.com>
2017-08-18 15:10:51 +0800

    add test api

--- flaskapp.py
+++ flaskapp.py
@@ -1,12 +1,16 @@
 # coding=utf-8
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'hello openshift'
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8080')
